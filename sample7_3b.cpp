9305420ddaf2f5ca2e60deca44a6c42af365e0b3
関口　裕基 <exx15285@sekiguchi-hiroki-no-MacBook-Air.local>
2017-10-01 17:56:02 +0900

    7_3章関数の引数（キーボードから読み出し）

---
+++ sample7_3b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+
+void buy ( int x , int y ){
+
+  cout << x << "万円と" << y << "の車を買いました。" << '\n' ;
+
+}
+
+int main () {
+
+  int num1 , num2 ;
+
+  std::cout << "いくらの車を買いました。" << '\n';
+  cin >> num1 ;
+
+  std::cout << "いくらの車を買いました。" << '\n';
+  cin >> num2 ;
+
+  buy ( num1 , num2 ) ;
+
+  return 0 ;
+
+}
